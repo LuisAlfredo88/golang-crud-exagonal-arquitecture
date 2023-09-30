5b0d3cde38365afae6525e6cbcdd99eb624c62b2
ldilone <luisalfredodilone@gmail.com>
2023-09-30 17:51:51 -0400

    Removing unnecessary module

--- internal/modules/product/application/service/get_products.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import (
-	"golang-crud-exagonal-arquitecture/internal/modules/product/model/entity"
-)
-
-func (l *product) GetAllProducts() ([]entity.Product, error) {
-	return l.productRepository.GetAllProducts()
-}
